1f22bb1fd99439c5aa193c9b03ec85eff84b4db0
Victor Torres <vpaivatorres@gmail.com>
2015-10-27 20:02:55 -0300

    Making setup.py work.

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+import io
+import os
+import re
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import relpath
+from os.path import splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+setup(
+    name='sinesp_client',
+    version='1.0',
+    license='Copyright',
+    description='SINESP Cidadão database client.',
+    author='Victor Torres',
+    author_email='vpaivatorres@gmai.com',
+    url='https://bitbucket.org/victortorres/sinesp-client',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'PySocks==1.5.6',
+    ]
+)
