91243d2f5534787a6a279f50033da581d54b39a4
Victor Torres <vpaivatorres@gmail.com>
2016-11-26 11:56:08 -0300

    Updating version.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='sinesp_client',
-    version='1.5',
+    version='1.6',
     license='Copyright',
     description='SINESP Cidadão database client.',
     author='Victor Torres',
